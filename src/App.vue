030fb09b2b6755f38eb12d6452c51f1a92229b13
AlexanderAthena <amroselfouly@gmail.com>
2024-04-17 08:23:41 +0300

    sync

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
+  <img style="width: 300px" alt="Vue logo" src="nbk_logo.jpeg">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,50 @@
+<script setup>
+import { ref } from 'vue';
+import { PDFDocument } from 'pdf-lib';
+
+const files = ref([]);
+const newPdfUrl = ref('');
+
+const processPDFs = async () => {
+    try {
+        const loadedDocs = await Promise.all(
+            files.value.map(async file => {
+                try {
+                    const fileData = new Uint8Array(await file.arrayBuffer());
+                    return await PDFDocument.load(fileData);
+                } catch (error) {
+                    console.error('Failed to load PDF:', error);
+                    return null; // Returning null for corrupted files
+                }
+            })
+        );
+
+        const newPdfDoc = await PDFDocument.create();
+        
+        for (const doc of loadedDocs) {
+            if (doc) { // Check if the doc is not null (corrupted)
+                const [firstPage] = await newPdfDoc.copyPages(doc, [0]);
+                newPdfDoc.addPage(firstPage);
+            }
+        }
+
+        const pdfBytes = await newPdfDoc.save();
+        const blob = new Blob([pdfBytes], { type: 'application/pdf' });
+        newPdfUrl.value = URL.createObjectURL(blob);
+    } catch (error) {
+        console.error('Error processing PDFs:', error);
+    }
+};
+</script>
+
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <input type="file" multiple accept="application/pdf" @change="files = [...$event.target.files]" />
+    <button @click="processPDFs">Generate PDF</button>
+    <a v-if="newPdfUrl" :href="newPdfUrl" download="combined.pdf">Download Combined PDF</a>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+/* Add your styling here */
 </style>
